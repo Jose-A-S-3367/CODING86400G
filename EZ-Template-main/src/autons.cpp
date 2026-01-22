4bbca99307b5c791be187e659ebe177070968690
Jose-A-S-3367 <jarguelless3367@gmail.com>
2026-01-22 08:35:40 -0500

    v.11.2

--- EZ-Template-main/src/autons.cpp
+++ EZ-Template-main/src/autons.cpp
@@ -580,7 +580,7 @@ void skills() {
   chassis.pid_turn_set(-180_deg, TURN_SPEED);
   chassis.pid_wait();
 
-  chassis.pid_drive_set(13_in, DRIVE_SPEED, true);
+  chassis.pid_drive_set(12.5_in, DRIVE_SPEED, true);
   chassis.pid_wait();
   
   chassis.pid_turn_set(-90_deg, TURN_SPEED);
